a8648932e7f9cf6b4de8333e1ee31d0af40d90e4
kshitizdona <kshitizb87@gmial.com>
2025-02-03 16:41:50 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const expressLayouts = require('express-ejs-layouts');
+
+// Connect to Database
+const MONGO_URL = "mongodb://127.0.0.1:27017/resultwave";
+
+main()
+    .then(() => {
+        console.log("Connected to DB");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+// Middlewares
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(expressLayouts);
+
+// Routes
+app.get("/", (req, res) => {
+    res.render("products/home.ejs");
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log("Server is listening to port 3000");
+});
